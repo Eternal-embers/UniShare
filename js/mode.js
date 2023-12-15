37a3dbbc0459bbb8915acfdf2e7bc5b6c2cde572
ember <laiqw6537@qq.com>
2023-12-15 23:02:20 +0800

    添加推荐模块的暗色主题

--- js/mode.js
+++ js/mode.js
@@ -1,18 +1,22 @@
 /* 获取body对象 */
 var body;
 var header;
+var recommand;
 document.addEventListener('DOMContentLoaded', function () {
     body = document.body;
     header = document.querySelector('.header');
+    recommand = document.querySelector('.recommand');
 });
 
 /* 切换到暗色主题 */
 function night_mode() {
     body.classList.add('night-mode');
     header.classList.add('header-dark');
+    recommand.classList.add('recommand-dark');
 }
 
 function light_mode() {
     body.classList.remove('night-mode');
     header.classList.remove('header-dark');
+    recommand.classList.remove('recommand-dark');
 }
