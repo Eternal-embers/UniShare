ebbf7d1e8e7d3c5aa8bcbf27f8f00b83fd63c5a5
ember <laiqw6537@qq.com>
2023-12-16 11:39:25 +0800

    添加footer的暗色主题

--- js/mode.js
+++ js/mode.js
@@ -2,10 +2,12 @@
 var body;
 var header;
 var recommand;
+var footer;
 document.addEventListener('DOMContentLoaded', function () {
     body = document.body;
     header = document.querySelector('.header');
     recommand = document.querySelector('.recommand');
+    footer = document.querySelector('.footer');
 });
 
 /* 切换到暗色主题 */
@@ -13,10 +15,12 @@ function night_mode() {
     body.classList.add('night-mode');
     header.classList.add('header-dark');
     recommand.classList.add('recommand-dark');
+    footer.classList.add('footer-dark');
 }
 
 function light_mode() {
     body.classList.remove('night-mode');
     header.classList.remove('header-dark');
     recommand.classList.remove('recommand-dark');
+    footer.classList.remove('footer-dark');
 }
